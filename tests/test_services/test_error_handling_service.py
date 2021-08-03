1a01d7f186bf930089ccb3b784d51a8923bd1fb2
alancta <chuan2@wisc.edu>
2021-08-03 18:56:57 -0500

    removed invalid test

--- tests/test_services/test_error_handling_service.py
+++ tests/test_services/test_error_handling_service.py
@@ -6,16 +6,6 @@ from requests.models import Response
 
 
 class TestErrorHandlingService(unittest.TestCase):
-
-    def test_ticket_not_found(self):
-        response = Response()
-        response.status_code = 404
-        error_handling_service = ErrorHandlingService()
-        with self.assertRaises(SystemExit) as cm:
-            with self.assertRaises(TicketException):
-                error_handling_service.load_ticket_handler(response)
-            self.assertEqual(cm.exception.code, 1)
-
     def test_authentication_error(self):
         response = Response()
         response.status_code = 401
