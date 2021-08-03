06e2858a7ee3b40cd76db133c235079de48c4f23
alancta <chuan2@wisc.edu>
2021-08-03 10:49:18 -0500

    added comments for error handling services

--- services/error_handling_service.py
+++ services/error_handling_service.py
@@ -1,15 +1,20 @@
 
 from exceptions import TicketException
-from requests.exceptions import RequestException
 import sys
 
 
 class ErrorHandlingService:
+    '''
+    A class used to manage errors.
+    '''
 
     def __init__(self):
         pass
 
     def load_ticket_handler(self, response):
+        '''
+        Handles response-related ticket errors.
+        '''
         if response.status_code == 401:
             print(
                 'Oops! Authorization failed. Please check your credentials and restart the program.')
@@ -22,9 +27,15 @@ class ErrorHandlingService:
         sys.exit(1)
 
     def timeout_error_handler(self):
+        '''
+        Handles TimeOutErrors, requests taking too long - likely connection error.
+        '''
         print('Request took too long to respond and timed out, please check your internet connection.')
         sys.exit(1)
 
     def connection_error_handler(self):
+        '''
+        Handles any connection errors. 
+        '''
         print('There was a problem with sending the request to the Tickets API, please check your connection and restart the program.')
         sys.exit(1)
